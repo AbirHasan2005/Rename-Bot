8517decf2d8d35ffdb8abd82218585b17fdbbb92
Abir Hasan <hackitabirhasan@gmail.com>
2022-02-06 01:00:58 +0600

    flood handler

--- bot/core/db/add.py
+++ bot/core/db/add.py
@@ -10,7 +10,7 @@ async def add_user_to_database(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
         if Config.LOG_CHANNEL is not None:
-            await bot.send_message(
+            await bot.send_flooded_message(
                 int(Config.LOG_CHANNEL),
                 f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
             )
