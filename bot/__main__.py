730fdab830dc20cb8492a434648f3159c0aae6ab
Abir Hasan <hackitabirhasan@gmail.com>
2022-02-25 12:30:03 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,33 @@
 # (c) @AbirHasan2005
 
+import os
+import sys
 import bot
+import shutil
+from pyrogram import idle
+from configs import Config
+from sqlite3 import OperationalError
+
+def run_bot():
+    bot.bot.start()
+    idle()
+
+
+def rm_st():
+    try: shutil.rmtree(Config.DOWNLOAD_DIR)
+    except: pass
+    try: os.remove("RenameBot.session")
+    except: pass
+    try: os.remove("RenameBot.session-journal")
+    except: pass
 
 
 if __name__ == "__main__":
-    bot.bot.run()
+    try:
+        run_bot()
+        bot.bot.stop()
+    except OperationalError:
+        try: bot.bot.stop()
+        except: pass
+        rm_st()
+        os.execl(sys.executable, sys.executable, *sys.argv)
